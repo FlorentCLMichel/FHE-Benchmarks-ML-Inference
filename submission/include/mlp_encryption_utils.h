14f9dbbe58c915668dc41bd0aa016cd925423c97
Shruthi Gorantala <gshruthi@google.com>
2025-08-11 16:16:50 +0000

    Add server side model quality computation binary and mlp_encryption_utils

---
+++ submission/include/mlp_encryption_utils.h
@@ -0,0 +1,31 @@
+#ifndef MLP_ENCRYPTION_UTILS_H_
+#define MLP_ENCRYPTION_UTILS_H_
+
+#include "openfhe/pke/openfhe.h"
+#include "params.h"
+
+using namespace lbcrypto;
+using CiphertextT = ConstCiphertext<DCRTPoly>;
+using CCParamsT = CCParams<CryptoContextCKKSRNS>;
+using CryptoContextT = CryptoContext<DCRTPoly>;
+using EvalKeyT = EvalKey<DCRTPoly>;
+using PlaintextT = Plaintext;
+using PrivateKeyT = PrivateKey<DCRTPoly>;
+using PublicKeyT = PublicKey<DCRTPoly>;
+
+#define MNIST_DIM 784
+#define NORMALIZED_DIM 1024
+
+struct Sample {
+  int label;
+  float image[NORMALIZED_DIM];
+};
+
+ConstCiphertext<DCRTPoly> encrypt_input(CryptoContext<DCRTPoly> cc, std::vector<float> input, PublicKey<DCRTPoly> pk);
+std::vector<float> mlp_decrypt(CryptoContextT v11343, CiphertextT v11344, PrivateKeyT v11345);
+PublicKey<DCRTPoly> read_public_key(const InstanceParams& prms);
+CryptoContext<DCRTPoly> read_crypto_context(const InstanceParams& prms);
+void load_dataset(std::vector<Sample> &dataset, const char *filename);
+int argmax(float *A, int N);
+
+#endif  // ifndef MLP_ENCRYPTION_UTILS_H_
--- submission/src/client_encode_encrypt_input.cpp
+++ submission/src/client_encode_encrypt_input.cpp
@@ -7,21 +7,9 @@
 #include "scheme/ckksrns/ckksrns-ser.h"
 #include "params.h"
 
+#include "mlp_encryption_utils.h"
+
 using namespace lbcrypto;
-#define MNIST_DIM 784
-#define NORMALIZED_DIM 1024
-
-struct Sample {
-  int label;
-  float image[NORMALIZED_DIM];
-};
-
-ConstCiphertext<DCRTPoly> encrypt_input(CryptoContext<DCRTPoly> cc, std::vector<float> input, PublicKey<DCRTPoly> pk);
-PublicKey<DCRTPoly> read_public_key(const InstanceParams& prms);
-CryptoContext<DCRTPoly> read_crypto_context(const InstanceParams& prms);
-void load_dataset(std::vector<Sample> &dataset, const char *filename);
-
-
 
 
 int main(int argc, char* argv[]){
@@ -35,6 +23,8 @@ int main(int argc, char* argv[]){
     InstanceParams prms(size);
 
     CryptoContext<DCRTPoly> cc = read_crypto_context(prms);
+
+    // Step 2: Read public key
     PublicKey<DCRTPoly> pk = read_public_key(prms);
 
     std::vector<Sample> dataset;
@@ -51,54 +41,4 @@ int main(int argc, char* argv[]){
     Serial::SerializeToFile(prms.ctxtupdir()/"cipher_input.bin", ctxt, SerType::BINARY);
 
     return 0;
-}
-
-PublicKey<DCRTPoly> read_public_key(const InstanceParams& prms) {
-    PublicKey<DCRTPoly> pk;
-    if (!Serial::DeserializeFromFile(prms.pubkeydir()/"pk.bin", pk,
-                                    SerType::BINARY)) {
-        throw std::runtime_error("Failed to get public key from  " + prms.pubkeydir().string());
-    }
-    return pk;
-}
-
-CryptoContext<DCRTPoly> read_crypto_context(const InstanceParams& prms) {
-    CryptoContext<DCRTPoly> cc;
-    if (!Serial::DeserializeFromFile(prms.pubkeydir()/"cc.bin", cc, SerType::BINARY)) {
-        throw std::runtime_error("Failed to get CryptoContext from " + prms.pubkeydir().string());
-    }
-    return cc;
-}
-
-
-ConstCiphertext<DCRTPoly> encrypt_input(CryptoContext<DCRTPoly> cc, std::vector<float> input, PublicKey<DCRTPoly> pk) {
-  std::vector<double> v11340(std::begin(input), std::end(input));
-  uint32_t v11340_filled_n = cc->GetCryptoParameters()->GetElementParams()->GetRingDimension() / 2;
-  auto v11340_filled = v11340;
-  v11340_filled.clear();
-  v11340_filled.reserve(v11340_filled_n);
-  for (uint32_t i = 0; i < v11340_filled_n; ++i) {
-    v11340_filled.push_back(v11340[i % v11340.size()]);
-  }
-  const auto& v11341 = cc->MakeCKKSPackedPlaintext(v11340_filled);
-  const auto& v11342 = cc->Encrypt(pk, v11341);
-  return v11342;
-}
-
-
-void load_dataset(std::vector<Sample> &dataset, const char *filename) {
-  std::ifstream file(filename);
-  Sample sample;
-  while (file >> sample.label) {
-    // Read MNIST_DIM values from file
-    for (int i = 0; i < MNIST_DIM; i++) {
-      file >> sample.image[i];
-    }
-    // Pad remaining values with 0.0 if NORMALIZED_DIM > MNIST_DIM
-    for (int i = MNIST_DIM; i < NORMALIZED_DIM; i++) {
-      sample.image[i] = 0.0f;
-    }
-
-    dataset.push_back(sample);
-  }
 }
